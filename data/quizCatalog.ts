65f26add2427cdc0dc04cf7bd54eec62ff9d686d
ekosh02 <polumontum@gmail.com>
2025-05-03 06:51:46 +0500

    change data

--- data/quizCatalog.ts
+++ data/quizCatalog.ts
@@ -5,31 +5,7 @@ export const quizCatalog: QuizCatalog = {
   data: [
     {
       id: '1',
-      name: 'Угадай музыку на скорость!',
-      description: 'Ответь на как можно больше вопросов за ограниченное время.',
-      hasTimer: true,
-      maxTimeSecond: 10,
-      image: plateGif,
-    },
-    {
-      id: '2',
-      name: 'Угадай музыку на скорость!',
-      description: 'Ответь на как можно больше вопросов за ограниченное время.',
-      hasTimer: true,
-      maxTimeSecond: 10,
-      image: plateGif,
-    },
-    {
-      id: '3',
-      name: 'Угадай музыку на скорость!',
-      description: 'Ответь на как можно больше вопросов за ограниченное время.',
-      hasTimer: true,
-      maxTimeSecond: 10,
-      image: plateGif,
-    },
-    {
-      id: '4',
-      name: 'Угадай музыку на скорость!',
+      name: 'Угадай музыку!',
       description: 'Ответь на как можно больше вопросов за ограниченное время.',
       hasTimer: true,
       maxTimeSecond: 10,
